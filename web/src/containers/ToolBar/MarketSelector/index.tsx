4a72308a4635a548673863e1af1d7faeaab7dbdf
Araviel-IO <contact@araviel.io>
2021-11-23 01:07:02 +0100

    floating market list styling

--- web/src/containers/ToolBar/MarketSelector/index.tsx
+++ web/src/containers/ToolBar/MarketSelector/index.tsx
@@ -36,7 +36,7 @@ class MarketSelectorComponent extends React.Component<ReduxProps, State> {
         const { searchFieldValue, marketsTabsSelectedValue } = this.state;
 
         const listClassName = classnames({
-            'pg-trading-header-selector-list-container-open': isOpen,
+            'pg-trading-header-selector-list-container-open-c': isOpen,
             'pg-trading-header-selector-list-container-close': !isOpen,
         });
         const searchSelectorClassName = classnames({
@@ -48,22 +48,17 @@ class MarketSelectorComponent extends React.Component<ReduxProps, State> {
             <div className="pg-trading-header-selector-container">
                 <div className={listClassName}>
                     <div className="pg-trading-header-selector-searchc-container">
-                        <div className="cr-title-component">Market</div>
-                        <input
-                            className="pg-trading-header-selector-search-field"
-                            onChange={this.searchFieldChangeHandler}
-                            value={searchFieldValue}
-                        />
+                        <div className="pg-trading-header-selector-searchc-container-title">Select market</div>
+                        <div>
+                            <input
+                                className="pg-trading-header-selector-searchc-field"
+                                onChange={this.searchFieldChangeHandler}
+                                value={searchFieldValue}
+                            />
+                        </div>
                     </div>
                     <MarketsTabs onSelect={this.marketsTabsSelectHandler} />
                     <MarketsList search={searchFieldValue} currencyQuote={marketsTabsSelectedValue} />
-                    <div className={'pg-trading-header-selector-search-wrapper'}>
-                        <div className={searchSelectorClassName}>
-                            <div className="pg-trading-header-selector-search-icon">
-                                <img alt="" src={searchIcon} />
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         );
